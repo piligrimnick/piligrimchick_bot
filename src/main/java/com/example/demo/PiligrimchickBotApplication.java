3a4258c425610a2dbc032280416740f91ce88a15
Nikita Bogomolov <nbogomolov@taxdome.com>
2025-03-09 21:34:59 +0100

    first commit

---
+++ src/main/java/com/example/demo/PiligrimchickBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiligrimchickBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiligrimchickBotApplication.class, args);
+	}
+
+}
