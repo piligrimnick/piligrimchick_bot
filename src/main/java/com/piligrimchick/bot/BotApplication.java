36b0deaf1a6634a383f51bf7a43d2faa96584bd4
Nikita Bogomolov <nbogomolov@taxdome.com>
2025-03-10 20:52:28 +0100

    Move and rename components

---
+++ src/main/java/com/piligrimchick/bot/BotApplication.java
@@ -0,0 +1,13 @@
+package com.piligrimchick.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.piligrimchick.bot", "com.piligrimchick.shared"})
+public class BotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BotApplication.class, args);
+    }
+}
